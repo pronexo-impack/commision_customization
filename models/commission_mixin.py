1bf5aa804dbc1bd4aa0014482d027192be9569e2
Nelson Tontarelli <nelartz01@gmail.com>
2024-03-15 11:16:23 -0300

    Comision del 0.5% a los agentes genericos

--- models/commission_mixin.py
+++ models/commission_mixin.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
-
+from odoo.exceptions import ValidationError
 
 class CommissionMixinExtended(models.AbstractModel):
     _inherit = "commission.mixin"
@@ -17,13 +17,18 @@ class CommissionMixinExtended(models.AbstractModel):
     def _prepare_agents_vals_partner(self, partner, settlement_type=None):
         """Utility method for getting agents creation dictionary of a partner."""
         agents = partner.agent_ids
+        comercial_id = self.order_id.user_id.commercial_partner_id
         commission_05id = self.env["commission"].search(
             [("name", "=", "Commission 0.5%"), ("fix_qty", "=", "0.5")], limit=1, order="id desc"
         )
-        if partner.generic_agent:
-            return [(0, 0, self._prepare_agent_vals_commission05(agent, commission_05id.id)) for agent in agents]
+        for ag in partner.agent_ids:
+            if ag.generic_agent:
+                if comercial_id.agent:
+                    return [(0, 0, self._prepare_agent_vals_commission05(comercial_id, commission_05id.id))]
+                else:
+                    return [(0, 0, self._prepare_agent_vals(agent)) for agent in agents]
 
-        elif settlement_type:
+        if settlement_type:
             agents = agents.filtered(
                 lambda x: not x.commission_id.settlement_type or x.commission_id.settlement_type == settlement_type
             )
